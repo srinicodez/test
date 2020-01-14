4f380e64466f805148f92e03a6fbe25a22fdabab
codeonly05 <codeonly05@gmail.com>
2020-01-14 23:26:19 +1100

    second

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mul
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 4;
+
+            int b = 5;
+
+            int c = a * b;
+
+            Console.WriteLine(c);
+
+        }
+    }
+}
